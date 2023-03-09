c54b01846297790dc93d2a560d21798888b4535e
MCortez <45056239+mc0x@users.noreply.github.com>
2023-03-09 11:59:25 -0800

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "2",
+    name: "Office Shirt",
+    price: 50,
+    desc: " dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "3",
+    name: "Mens Suit",
+    price: 100,
+    desc: "blah blah blah lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "4",
+    name: "Casual Shirt",
+    price: 30,
+    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+    img: "images/img-4.jpg",
+  },
+];
